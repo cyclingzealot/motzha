c176babe169eecc877479ea1da87c07a11b6cbfe
Julien Lamarche <julien.lamarche@gmail.com>
2021-01-26 00:30:41 -0500

    day of month printing automated

--- lib/motzha.rb
+++ lib/motzha.rb
@@ -1,8 +1,19 @@
 require 'erb'
+require 'date'
 
 class Motzha
 
     SVG_TEMPLATE_PATH = 'lib/motzha/erb/calendarGridSimple.svg.erb'
+
+    @month
+
+    def initialize(date)
+        setMonth(date)
+    end
+
+    def setMonth(date)
+        @month = (date - date.mday + 1)
+    end
 
 	def self.hi
         puts "Hello #{__LINE__}"
@@ -11,15 +22,20 @@ class Motzha
 	end
 
 
-    def self.printSvg
+    def printSvg
         template = File.read(SVG_TEMPLATE_PATH)
 
         e = ERB.new(template)
 
-        puts e.result()
+        puts e.result(binding)
 
     end
 
+
+    def self.test
+        t = new(Date.today)
+        t.printSvg
+    end
 
 
 end
