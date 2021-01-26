54eaaf9b2612501e73f191e998270692e9e248f5
Julien Lamarche <julien.lamarche@gmail.com>
2021-01-25 22:50:42 -0500

    remove rvmDO from compile.bash

--- compile.bash
+++ compile.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rvmDo gem build motzha.gemspec && rvmDo gem install ./motzha-0.0.1.gem  && echo && echo "require 'motzha'; Motzha.hi; " | rvmDo ruby  && echo
+gem build motzha.gemspec && gem install ./motzha-0.0.1.gem  && echo && echo "require 'motzha'; Motzha.printSvg; " | ruby  && echo
